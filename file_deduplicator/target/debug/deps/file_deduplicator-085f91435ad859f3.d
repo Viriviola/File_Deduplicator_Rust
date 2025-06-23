0d347e1e735a8afdd542bf3fdd828e5607eb12a5
Viriviola <heraalaka23@gmail.com>
2025-06-24 00:30:55 +0530

    feat(scan): implemented file collection using WalkDir

--- file_deduplicator/target/debug/deps/file_deduplicator-085f91435ad859f3.d
+++ file_deduplicator/target/debug/deps/file_deduplicator-085f91435ad859f3.d
@@ -1,5 +1,7 @@
-E:\File_Dedup_Rust\File_Dedup_Rust\file_deduplicator\target\debug\deps\libfile_deduplicator-085f91435ad859f3.rmeta: src\main.rs
+E:\File_Dedup_Rust\File_Dedup_Rust\file_deduplicator\target\debug\deps\libfile_deduplicator-085f91435ad859f3.rmeta: src\main.rs src\cli.rs src\scan.rs
 
-E:\File_Dedup_Rust\File_Dedup_Rust\file_deduplicator\target\debug\deps\file_deduplicator-085f91435ad859f3.d: src\main.rs
+E:\File_Dedup_Rust\File_Dedup_Rust\file_deduplicator\target\debug\deps\file_deduplicator-085f91435ad859f3.d: src\main.rs src\cli.rs src\scan.rs
 
 src\main.rs:
+src\cli.rs:
+src\scan.rs:
--- file_deduplicator/target/debug/deps/file_deduplicator-4e2553cbb0bb02b6.d
+++ file_deduplicator/target/debug/deps/file_deduplicator-4e2553cbb0bb02b6.d
@@ -1,5 +1,7 @@
-E:\File_Dedup_Rust\File_Dedup_Rust\file_deduplicator\target\debug\deps\libfile_deduplicator-4e2553cbb0bb02b6.rmeta: src\main.rs
+E:\File_Dedup_Rust\File_Dedup_Rust\file_deduplicator\target\debug\deps\libfile_deduplicator-4e2553cbb0bb02b6.rmeta: src\main.rs src\cli.rs src\scan.rs
 
-E:\File_Dedup_Rust\File_Dedup_Rust\file_deduplicator\target\debug\deps\file_deduplicator-4e2553cbb0bb02b6.d: src\main.rs
+E:\File_Dedup_Rust\File_Dedup_Rust\file_deduplicator\target\debug\deps\file_deduplicator-4e2553cbb0bb02b6.d: src\main.rs src\cli.rs src\scan.rs
 
 src\main.rs:
+src\cli.rs:
+src\scan.rs:
